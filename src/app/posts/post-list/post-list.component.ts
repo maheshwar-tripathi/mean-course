958a59d59ec1e24fe601197605569703259ecf5b
maheshwar-tripathi <maheshwar2005@gmail.com>
2023-04-23 05:47:30 +0530

    Added Posts services

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
   // posts = [
   //   {title: 'First Post', content: 'this is the first\'s post content'},
   //   {title: 'Second Post', content: 'this is the second\'s post content'},
@@ -15,11 +18,21 @@ export class PostListComponent implements OnInit {
   //   {title: 'Fifth Post', content: 'this is the fifth\'s post content'},
   // ];
 
-  @Input() posts: Post[] = [];
+  posts: Post[] = [];
+  private subscribePosts!: Subscription;
 
-  constructor() { }
+  constructor(public postsService: PostsService) { }
 
   ngOnInit(): void {
+    this.posts = this.postsService.getPosts();
+    this.subscribePosts = this.postsService.getPostUpdateListener()
+      .subscribe((updatePosts: Post[]) =>{
+        this.posts =updatePosts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.subscribePosts.unsubscribe();
   }
 
 }
